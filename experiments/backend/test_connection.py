77bd4fe4be822c18dee31bdb121f0de780cda713
Olga Kutueva <kutueva.olga@gmail.com>
2023-09-08 15:19:01 +0300

    Получение списка переменных окружения (по примеру)

--- experiments/backend/test_connection.py
+++ experiments/backend/test_connection.py
@@ -1,6 +1,13 @@
 from getpass import getpass
 from mysql.connector import connect, Error
 
+import os
+import pprint
+
+env_var = os.environ
+print("User's Environment variable:")
+pprint.pprint(dict(env_var), width = 1)
+
 try:
     with connect(
         host="localhost",
