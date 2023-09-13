55eaf968a5022d0ed6326f2e0d3dabcbe56db2ce
Olga Kutueva <kutueva.olga@gmail.com>
2023-09-13 11:50:43 +0300

    Исправлен файл инициализации sql

--- experiments/database/db_init.sql
+++ experiments/database/db_init.sql
@@ -6,3 +6,12 @@ CREATE TABLE IF NOT EXISTS users(
     password varchar(255) NOT NULL,
     PRIMARY KEY(id)
 );
+
+CREATE TABLE IF NOT EXISTS resumes(
+    id int NOT NULL AUTO_INCREMENT,
+    user_id int NOT NULL,
+    resume_title VARCHAR(100),
+    resume_text TEXT,
+    PRIMARY KEY(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
