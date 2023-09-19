3e9af26dcc6861f0ccd35c4f411a758350bb68ca
Olga Kutueva <kutueva.olga@gmail.com>
2023-09-19 15:26:28 +0300

    Отправка JSON-сообщения

--- experiments/backend/test_flask.py
+++ experiments/backend/test_flask.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello World!"
+
+@app.route('/json')
+def get_json():
+    return {"string value":"string field",
+            "number value": 123}
