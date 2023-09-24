2a1ac8c99211ec8b8c318836c75312237d917df2
Olga Kutueva <kutueva.olga@gmail.com>
2023-09-24 06:35:19 +0300

    Include, extends

--- experiments/backend/test-jinja.py
+++ experiments/backend/test-jinja.py
@@ -1,12 +1,20 @@
 import jinja2
 from jinja2 import Environment, FileSystemLoader
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return template.render(title = title, name = name)
 
 # инициализация среды выполнения для шаблонов 
 environment = Environment(loader=FileSystemLoader("templates/"))
-# загрузка шаблона
-template = environment.get_template("index.html")
+# загрузка шаблона body.html -> base.html
+template = environment.get_template("body.html")
 
 # визуализация шаблона
 title = "Page title"
 name = "John"
+# расширение базового шаблона body.html -> base.html
 print(template.render(title = title, name = name))
