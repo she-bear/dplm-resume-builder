2aa2cef2aa92a80e88ff9c53471deb72be3fde6b
Olga Kutueva <kutueva.olga@gmail.com>
2023-09-24 10:43:54 +0300

    Подключение jinja2 к flask

--- experiments/backend/test-jinja.py
+++ experiments/backend/test-jinja.py
@@ -4,10 +4,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return template.render(title = title, name = name)
-
 # инициализация среды выполнения для шаблонов 
 environment = Environment(loader=FileSystemLoader("templates/"))
 # загрузка шаблона body.html -> base.html
@@ -18,3 +14,11 @@ title = "Page title"
 name = "John"
 # расширение базового шаблона body.html -> base.html
 print(template.render(title = title, name = name))
+
+@app.route('/')
+def home():
+    return render_template("body.html", title = "Page title", name = "John")
+
+@app.route('/template/<int:id>')
+def get_user_page(id):
+    return render_template("welcome.html", id = id)
