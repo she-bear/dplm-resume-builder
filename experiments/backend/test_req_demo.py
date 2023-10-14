c55beebf0e49811c39ed5de56c2297cdd72067cf
Olga Kutueva <kutueva.olga@gmail.com>
2023-10-14 12:14:38 +0300

    Демонстрация post json и фоновых запросов с fetch

--- experiments/backend/test_req_demo.py
+++ experiments/backend/test_req_demo.py
@@ -1,6 +1,6 @@
 # передать данные param1=value1 param2=value2 четырьмя способами
 
-from flask import Flask, request, render_template
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
@@ -11,6 +11,7 @@ def home():
 # здесь можно задать значения по умолчанию
 # но нужно преобразование в int и проверка типов
 # запрос вида http://127.0.0.1:5000/get_param?param1=2&param2=10
+# тип передачи параметров: query
 @app.route("/get_param")
 def get_param():
   param1 = request.args.get("param1", 0)
@@ -20,12 +21,14 @@ def get_param():
 
 # для второго параметра задано значение по умолчанию
 # запрос вида http://127.0.0.1:5000/url/2_10
+# тип передачи параметров: path
 @app.route('/url/<int:param1>',  defaults={'param2': 0})
 @app.route('/url/<int:param1>_<int:param2>')
 def show_id(param1, param2):
     return f"ID = {param1}, ID = {param2}"
 
 # post form
+# тип передачи параметров: body, тип кодирования: form
 @app.route('/post_form', methods=['post', 'get'])
 def login():
     message = ''
@@ -35,3 +38,24 @@ def login():
         message = f'Hello, {username} with password={password}!'
    
     return render_template('login.html', message = message)
+
+# post json
+# запрос: GET
+# параметры: -
+# ответ: html
+
+# запрос: POST
+# параметры body json: 
+# - username : string
+# - password : string
+# ответ: json
+#   "message" : string
+@app.route('/post_json',  methods=['post', 'get'])
+def post_json():
+    if request.method == 'POST':
+        username = request.json.get('username')
+        password = request.json.get('password')
+        message = f'Hello, {username} with password={password}!'
+        return {"message" : message}
+    
+    return render_template('post_json.html')
