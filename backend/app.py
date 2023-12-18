9688fe03c88e03c2466dc1e9a347dd6057efa512
Olga Kutueva <kutueva.olga@gmail.com>
2023-12-18 18:40:25 +0200

    Регистрация пользователя: форма, передача параметров.

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello World!"
+@app.route("/register", methods=['post', 'get'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username', '', str)
+        password = request.form.get('password', '', str)
+        password_confirm = request.form.get('password_confirm', '', str)
+        print(username, password, password_confirm)
+        return 'ok'
+
+    else:
+        return render_template("register.html")
